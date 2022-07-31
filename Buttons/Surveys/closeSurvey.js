596fcaa09ebbdb6ba24da8ff2c0de088ee9f5c0c
Bartłomiej Wypart <41068795+dintees@users.noreply.github.com>
2022-07-31 10:03:05 +0200

    Improved surveys functionality

--- Buttons/Surveys/closeSurvey.js
+++ Buttons/Surveys/closeSurvey.js
@@ -1,4 +1,5 @@
 const { Modal, TextInputComponent, showModal } = require('discord-modals')
+const { MessageSelectMenu } = require('discord.js');
 
 module.exports = {
     id: "close-survey",
@@ -7,6 +8,14 @@ module.exports = {
         const modal = new Modal().setCustomId("close-survey-modal")
             .setTitle("Podsumowanie sugestii")
             .addComponents(
+                new MessageSelectMenu()
+                    .setCustomId("survey-closure-type")
+                    .setPlaceholder("Sposób zakończenia")
+                    .addOptions([
+                        { label: "Zgodnie z wynikami głosowania", description: "Rozpatrzenie zależy od wyników głosowania", value: "voting" },
+                        { label: "Zaakceptuj", description: "Akceptacja niezależnie od wyników głosowania", value: "acceptance" },
+                        { label: "Odrzuć", description: "Odrzucenie niezależnie od wyników głosowania", value: "rejection" },
+                    ]),
                 new TextInputComponent()
                     .setCustomId("survey-description")
                     .setLabel("Opis podjętych działań")
--- Events/Surveys/closeSurvey.js
+++ Events/Surveys/closeSurvey.js
@@ -8,43 +8,57 @@ module.exports = {
         if (modal.customId !== "close-survey-modal") return;
         await modal.deferReply({ ephemeral: true })
 
+        const closureType = modal.getSelectMenuValues('survey-closure-type')[0];
         const description = modal.getTextInputValue("survey-description");
 
         const suggestionsChannel = client.channels.cache.find(channel => channel.name === channelNames.userSuggestionsChannel);
         const messageID = modal.message.embeds[0].fields.find(f => f.name === "ID wiadomości").value;
 
         try {
+            // message from user channel
+            const message = await suggestionsChannel.messages.fetch(messageID);
+            const embed = message.embeds[0];
 
-            // a mesasge from user channel
-            const message = await suggestionsChannel.messages.fetch(messageID)
+            if (closureType === 'voting') {
+                const votesFor = message.reactions.cache.get('👍').count;
+                const votesAgainst = message.reactions.cache.get('👎').count;
 
-            const votesFor = message.reactions.cache.get('👍').count;
-            const votesAgainst = message.reactions.cache.get('👎').count;
+                embed.addFields(
+                    { name: 'Wyniki głosowania', value: '👍 - **' + votesFor.toString() + '** || 👎 - **' + votesAgainst.toString() + '**' },
+                    { name: 'Podsumowanie', value: '`' + description + '`' }
+                );
+                if (votesFor > votesAgainst) {
+                    embed.setColor("GREEN");
+                    embed.setThumbnail('https://i.imgur.com/Wv4YZwu.png');
+                }
+                else {
+                    embed.setColor("RED");
+                    // embed.setThumbnail('')
+                }
+            } else if (closureType === 'acceptance') {
+                embed.setColor("GREEN")
+                    .setThumbnail('https://i.imgur.com/Wv4YZwu.png')
+                    .addFields(
+                        { name: 'Pełna akceptacja sugestii przez', value: '`' + modal.user.username + '`' },
+                        { name: 'Podsumowanie', value: '`' + description + '`' }
+                    );
+            } else {
+                embed.addFields(
+                    { name: 'Odrzucenie sugestii przez', value: '`' + modal.user.username + '`' },
+                    { name: 'Podsumowanie', value: '`' + description + '`' }
+                )
+                    .setColor("RED");
+            }
 
             // remove all reactions
             await message.reactions.removeAll();
-
-            // edit message
-            const embed = message.embeds[0];
-
-            embed.addFields(
-                { name: 'Wyniki głosowania', value: '👍 - **' + votesFor.toString() + '** || 👎 - **' + votesAgainst.toString() + '**' },
-                { name: 'Podsumowanie', value: '`' + description + '`' }
-            );
-            if (votesFor > votesAgainst) {
-                embed.setColor("GREEN");
-                embed.setThumbnail('https://i.imgur.com/Wv4YZwu.png');
-            }
-            else {
-                embed.setColor("RED");
-                // embed.setThumbnail('')
-            }
 
             await message.edit({ embeds: [embed] });
             await modal.followUp({ embeds: [new MessageEmbed().setDescription('Wiadomość na kanale `@' + channelNames.userSuggestionsChannel + '` została edytowana.').setColor("GREEN")] })
             await modal.message.delete();
         } catch (err) {
-            await modal.followUp({ embeds: [new MessageEmbed().setDescription("Coś poszło nie tak...").setColor("RED")] });
+            console.log(err);
+            await modal.reply({ embeds: [new MessageEmbed().setDescription("Coś poszło nie tak...").setColor("RED")], ephemeral: true });
         }
     }
 }
